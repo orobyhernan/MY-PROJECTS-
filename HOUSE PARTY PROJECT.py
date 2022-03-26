9991d99ed44d69b28d34ad7b713033584ff86f50
orobyhernan <88052781+orobyhernan@users.noreply.github.com>
2022-03-26 19:18:21 +0100

    Add files via upload
    So I wanna have a party at my house, but given that is a big-ass house and a lotta people are coming, I´ll have to set some rules, different for each room inside the house and inform all guests of how much they will have to pay, let´s experiment with loops, dictionaries and functions to set said rules.

--- HOUSE PARTY PROJECT.py
+++ HOUSE PARTY PROJECT.py
@@ -1,3 +1,5 @@
+#WARNING: ALL PRINT AND EXECUTABLE FUNCTIONS WILL BE NAMED WITH AN # IF YOU WANT TO COPY THE CODE ANT RUN IT ON YOUR
+#IDE, JUST DELETE THE # AND RUN IT.
 #So I am rich and I am building my new house, I decided to make a list with all
 #the parts from the new house and their corresponding areas
 
@@ -6,10 +8,10 @@ areas = ["hallway", 11.25, "kitchen", 18.0,]
 #oops !! looks like the list is incomplete, I won´t be able
 # to throw parties with such a small house. Let´s add more elements with the add function
 
-bigger_areas = areas + ["living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+bigger_areas = areas + ["living room", 20.0, "bedroom", 10.75, "bathroom", 9.50, "attic", 14]
 
 
-#o no !!! how are we gonna throw parties with such a small living room ? plus,
+#oH no !!! how are we gonna throw parties with such a small living room ? plus,
 #we need a cooler name for that part of the house, "chill zone" will definitely attract more people
 
 bigger_areas[5] = 40.0
@@ -18,14 +20,33 @@ bigger_areas[4] = "Chill zone"
 #in order to throw parties, there will be placards on each door, indicating
 #which room has an area bigger than 15 square meters. if it´s bigger the placard will say "party friendly"
 #if it´s not the placard will say "no party here"
+#first of all let´s create a dictionary with the same list
 
-bigger_areas_copy = list(bigger_areas)
+bigger_areas = {"hallway": 11.25, "kitchen": 18.0, "living room": 20.0, "bedroom": 10.75, "bathroom": 9.50, "attic": 14,
+                "garden": 45}
 
-bigger_areas_copy[1] = "no party here"
-bigger_areas_copy[3] = "party friendly"
-bigger_areas_copy[5] = "party friendly"
-bigger_areas_copy[7] = "no party here"
-bigger_areas_copy[9] = "no party here"
-print(bigger_areas_copy)
-print(bigger_areas)
+#now let´s make a loop in order to display in which rooms can we party (or not) and how much we´ll have to
+#pay to party in each room, the price will vary depending on the surface
+
+def is_party_allowed():
+    for key in bigger_areas.keys():
+        if bigger_areas[key] > 15 and bigger_areas[key] < 20:
+           print(f"The {key} is party friendly and you´ll have to pay 40$ to party here")
+        if bigger_areas[key] >= 20 and bigger_areas[key] < 30:
+           print(f"The {key} is party friendly and you´ll have to pay 50$ to party here")
+        if bigger_areas[key] >= 30:
+           print(f"The {key} is party friendly and you´ll have to pay 60$ to party here")
+        elif bigger_areas[key] < 15:
+           print(f"You CANNOT party in the {key} so you won´t have to pay a dime")
+
+print(is_party_allowed())
+
+
+
+
+
+
+
+
+
 
